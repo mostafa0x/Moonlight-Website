7598b89c5567d7e0ddcba7e3f463778573965424
mostafa0x <dodomido12350@gmail.com>
2026-04-24 08:28:22 -0700

    feat: add multi-language support and implement BestSeller component

--- src/features/packages/components/BestSeller.tsx
+++ src/features/packages/components/BestSeller.tsx
@@ -1,13 +1,17 @@
+"use client";
+
 import { memo } from "react"
+import { useTranslations } from "next-intl";
 
 function BestSeller() {
+    const t = useTranslations("home");
     return (
         <div className=" absolute left-2 top-4 lg:top-5.5 z-5">
             <span className="bg-[#262626] w-18 h-7.75 lg:w-22.75 lg:h-11 rounded-[20px] px-2 text-sm lg:text-base text-[#F2C975] text-nowrap align-middle items-center justify-center">
-                Best Seller
+                {t("bestSeller")}
             </span>
         </div>
     )
 }
 
-export default memo(BestSeller)
+export default memo(BestSeller)
