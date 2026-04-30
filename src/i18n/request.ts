1de26d05e8a65fee0d1854e9738bbb91c16ad3a0
mostafa0x <dodomido12350@gmail.com>
2026-04-29 21:59:45 -0700

    feat: implement next-intl request configuration for locale handling

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,4 @@
-import { cookies, headers } from "next/headers";
-import { getRequestConfig, getLocale } from "next-intl/server";
+import { getRequestConfig } from "next-intl/server";
 
 export default getRequestConfig(async ({ locale }) => {
   if (!locale) {
